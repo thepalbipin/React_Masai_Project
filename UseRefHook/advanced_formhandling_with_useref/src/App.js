a1b893e04e05b857eb2e60c7d737cc5618a70e57
Bipin <bipinpal737@gmail.com>
2024-06-01 01:49:19 +0530

    Advanced Form Focus Input

---
+++ UseRefHook/advanced_formhandling_with_useref/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import React, {useRef, useEffect } from 'react';
+
+function App() {
+
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const messageInputRef = useRef(null);
+
+  useEffect(()=>{
+    nameInputRef.current.focus();
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(nameInputRef.current.value);
+    console.log(emailInputRef.current.value);
+    console.log(messageInputRef.current.value);
+  }
+
+  return (
+    <div className="App">
+      <form>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input
+            placeholder='Enter Name'
+            type='text'
+            id='name'
+            ref={nameInputRef}
+          />
+          <label htmlFor='email'>Enter Email</label>
+          <input
+            placeholder='Enter Email'
+            type='text'
+            id='email'
+            ref={emailInputRef}
+          />
+          <label htmlFor='message'>Enter Message</label>
+          <input
+            placeholder='Enter Message'
+            type='text'
+            id='message'
+            ref={messageInputRef}
+          />
+          <button type='submit' onClick={handleSubmit}>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
